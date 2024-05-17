43b7819730d9ac2d2f8e4e933b2b00b05b43513c
Kotya556 <timonin060@gmail.com>
2024-05-17 19:55:28 +1000

    Добавление сервера

---
+++ Content.Client/Corvax/TTS/HumanoidProfileEditor.TTS.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using Content.Client.Lobby;
+using Content.Corvax.Interfaces.Shared;
+using Content.Shared.Corvax.TTS;
+using Content.Shared.Preferences;
+
+namespace Content.Client.Preferences.UI;
+
+public sealed partial class HumanoidProfileEditor
+{
+    private ISharedSponsorsManager? _sponsorsMgr;
+    private List<TTSVoicePrototype> _voiceList = new();
+
+    private void InitializeVoice()
+    {
+        _voiceList = _prototypeManager
+            .EnumeratePrototypes<TTSVoicePrototype>()
+            .Where(o => o.RoundStart)
+            .OrderBy(o => Loc.GetString(o.Name))
+            .ToList();
+
+        _voiceButton.OnItemSelected += args =>
+        {
+            _voiceButton.SelectId(args.Id);
+            SetVoice(_voiceList[args.Id].ID);
+        };
+
+        _voicePlayButton.OnPressed += _ => { UserInterfaceManager.GetUIController<LobbyUIController>().PlayTTS(); };
+        IoCManager.Instance!.TryResolveType(out _sponsorsMgr);
+    }
+
+    private void UpdateTTSVoicesControls()
+    {
+        if (Profile is null)
+            return;
+
+        _voiceButton.Clear();
+
+        var firstVoiceChoiceId = 1;
+        for (var i = 0; i < _voiceList.Count; i++)
+        {
+            var voice = _voiceList[i];
+            if (!HumanoidCharacterProfile.CanHaveVoice(voice, Profile.Sex))
+                continue;
+
+            var name = Loc.GetString(voice.Name);
+            _voiceButton.AddItem(name, i);
+
+            if (firstVoiceChoiceId == 1)
+                firstVoiceChoiceId = i;
+
+            if (_sponsorsMgr is null)
+                continue;
+            if (voice.SponsorOnly && _sponsorsMgr != null &&
+                !_sponsorsMgr.GetClientPrototypes().Contains(voice.ID))
+            {
+                _voiceButton.SetItemDisabled(_voiceButton.GetIdx(i), true);
+            }
+        }
+
+        var voiceChoiceId = _voiceList.FindIndex(x => x.ID == Profile.Voice);
+        if (!_voiceButton.TrySelectId(voiceChoiceId) &&
+            _voiceButton.TrySelectId(firstVoiceChoiceId))
+        {
+            SetVoice(_voiceList[firstVoiceChoiceId].ID);
+        }
+    }
+}
